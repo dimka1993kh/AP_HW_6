95b2d8fd05be729cd87b85413048c40e6769a42e
dimka1993kh <dimka1993kh@gmail.com>
2021-02-28 18:38:10 +0300

    Добавлен поиск по превью, назаванию и полному тексту статьи

--- main.py
+++ main.py
@@ -17,30 +17,37 @@ soup = BeautifulSoup(text, features="html.parser" )
 articles = soup.findAll('article', class_='post')
 # В каждом посте произведем проверку на равенство любого ключевого слова и тэга. При совпадении - записываем дату, заголовок и ссылку на пост.
 for index, i in enumerate(articles):
+    # Найдем полный текст статьи
+    post_url = i.find('a', class_='post__title_link').get('href')
+    resp = requests.get(post_url)
+    resp.raise_for_status()
+    post_text_resp = resp.text
+    soup_post = BeautifulSoup(post_text_resp, features="html.parser")
+    post_text = soup_post.find('div', class_='post__body_full').text.lower().strip().split()
+    # Найдеи тэги, превью, заголовок
     hub = i.findAll('a', class_='hub-link')
-    hubs = [x for x in hub]
+    preview = i.find('div', class_='post__text').text.lower().strip().split()
+    title = i.find('a', class_='post__title_link').text.lower().strip().split()
+
+    hubs = [x.text.lower().strip().split() for x in hub] + preview + title + post_text
     let = False
     for hub in hubs:
-        if any(KEY == hub.text.lower().strip() for KEY in KEYWORDS):
-            let = True
+        for key in KEYWORDS:
+            if key == hub:
+                let = True
+                # print(key)
     if let:
         date = i.find('span', class_='post__time').text 
         title = i.find('h2', class_='post__title').text
         post_url = i.find('a', class_='post__title_link').get('href')
-        # Получим текст страницы поста и найдем там текст саомй статьи
-        resp = requests.get(post_url)
-        resp.raise_for_status()
-        post_text_resp = resp.text
-
-        soup_post = BeautifulSoup(post_text_resp, features="html.parser")
-        post_text = soup_post.find('div', class_='post__body_full').text
+        
         # Выведем полученные данные
         pprint({
-            f'{index} пост' :{
+            f'{index + 1} пост' :{
                             'date': date.strip(),
                             'title': title.strip(),
                             'post_url': post_url.strip(),
-                            'post_text': post_text.strip()
+                            # 'post_text': post_text.strip()
                              }
         })
         print()
